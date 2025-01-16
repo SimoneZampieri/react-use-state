3de63b9f9c0700cf5806338d67b9fc3454228577
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-16 15:27:25 +0100

    [update] main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 const App = () => {
   return (
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import languages from "../assets/languages";
+
+const Main = () => {
+  <main>
+    <section className="buttons">
+      <button>CIAO SONO UN BUTUN</button>
+    </section>
+  </main>;
+};
+
+export default Main;
