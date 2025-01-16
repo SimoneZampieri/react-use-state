c39906952c3d58af2bed05b15056c6e36a315426
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-16 15:53:04 +0100

    [update] added hooks

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,20 @@
 import languages from "../assets/languages";
+import { useState } from "react";
+
+function Main() {
+  const [langSelected, setLangSelected] = useState(0);
+  const [title, updateTitle] = useState("Nessun linguaggio selezionato");
+  const [desc, updateDesc] = useState("");
+}
 
 const Main = () => {
-  <main>
-    <section className="buttons">
-      <button>CIAO SONO UN BUTUN</button>
-    </section>
-  </main>;
+  return (
+    <main>
+      <div className="container py-4">
+        <div className="buttons-row"></div>
+      </div>
+    </main>
+  );
 };
 
 export default Main;
